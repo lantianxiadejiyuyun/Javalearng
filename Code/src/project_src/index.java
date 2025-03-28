5dd0e14ca92d35f96e9f2cf8f5283fb62b84de31
咸植宝 <2185888430@qq.com>
2025-03-28 16:12:22 +0800

    创建了bug

---
+++ Code/src/project_src/index.java
@@ -0,0 +1,10 @@
+public class index {
+
+    public static void main (String[] args) {
+        String args = "Hello World";
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
