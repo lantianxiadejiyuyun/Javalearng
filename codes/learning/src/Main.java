ff9ef862cb715ae88b1bb063552f964b818a0701
咸植宝 <2185888430@qq.com>
2025-03-31 10:04:03 +0800

    学习了 方法

---
+++ codes/learning/src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String Name = "Hello, World!";
+
+        for (int i = 0; i < Name.length(); i++) {
+            System.out.println(Name.charAt(i));
+        }
+
+        functionPrint("Hello, World！");
+        String data =  returnData("ReturnData");
+
+        System.out.println(data);
+    }
+
+    // 打印方法 关键字
+    // public static void 方法名 (传入值) 需要加入需要传入的类型 类似 TypeStript
+    public static void functionPrint(String name) {
+
+        System.out.println(name);
+    }
+
+    // 处理返回
+    // public static 返回值类型 方法名 （输入数据类型 传入值）
+    public static String returnData (String name) {
+
+        return name;
+    }
+}
