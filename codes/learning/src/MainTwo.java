8e1a58f0aa71f78ade982c057d3704889bb3e4d0
咸植宝 <2185888430@qq.com>
2025-03-31 11:18:34 +0800

    学习了 变量重新赋值

---
+++ codes/learning/src/MainTwo.java
@@ -0,0 +1,17 @@
+public class MainTwo {
+    public static void main (String[] args) {
+        // string 学习
+        String data = "checkData";
+
+        System.out.println(data);
+
+        data = "xiugai";
+
+        System.out.println(data);
+        //变量可以被修改
+
+
+
+
+    }
+}
